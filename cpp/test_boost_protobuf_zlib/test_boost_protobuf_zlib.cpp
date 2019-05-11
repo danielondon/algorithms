2efc3164cd1c4c79aa3a92957d65c4877cf76f4d
Daniel Londono <danielondon@gmail.com>
2019-05-11 12:19:11 +0200

    test dependencies of protobuf zlib and boost

---
+++ cpp/test_boost_protobuf_zlib/test_boost_protobuf_zlib.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <zlib.h>
+#include <test_boost_protobuf_zlib/tripcommon.pb.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Test for zlib, protobug and valhalla"<<endl<<endl;
+    //::google::protobuf::uint32 begin_heading;
+    return 0;
+}
